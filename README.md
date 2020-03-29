4291bd586f4f6c9afb5f800faeb22439aa109f54
zhaitiantian <17801153348@163.com>
2020-03-29 11:10:27 +0800

    新增计算类

---
+++ README.md
@@ -0,0 +1,6 @@
+##介绍
+#我的第一个用pytest创建的测试case
+
+##Quick start
+##参考链接
+
--- base_calcu/calcu.py
+++ base_calcu/calcu.py
@@ -1,2 +1,12 @@
 class Cacul():
-    pass
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a: float, b: float) -> float:
+        return a - b
+
+    def div(self, a: tuple) -> float:
+        return a[0] / a[1]
+
+    def muti(self, a: tuple) -> float:
+        return a[0] * a[1]
